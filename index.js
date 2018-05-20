981ca876500b9941401b83b37cb8249340975c0a
Danielle.Leblanc-Cyr <danielleleb12@gmail.com>
2018-05-20 00:00:54 -0300

    mobile responsive

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 'use strict';
 
+const mobileNavA = document.querySelector('.mobile-nav');
+const fullNav = document.querySelector('.main-nav');
+const navLink = document.querySelector('.main-nav__item');
+const mobileExit = document.querySelector('.mobile-exit');
+const portfolioImages = document.querySelectorAll('.image-grid__square');
+
+const hideNav = function() {
+    fullNav.classList.add('main-nav-hidden');
+};
+
+mobileExit.addEventListener('click', () => hideNav());
+
+mobileNavA.addEventListener('click', function() {
+    fullNav.classList.remove('main-nav-hidden');
+})
+
+navLink.addEventListener('click', () => hideNav());
+
+// portfolioImages.forEach(image => {
+
+// })
+
